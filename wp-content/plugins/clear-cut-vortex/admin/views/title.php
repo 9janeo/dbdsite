9437d3f1d05373dff48d6de971961552cbae6961
Matt Adewole <matt.adewole@gmail.com>
2023-03-29 00:58:49 -0500

    begin custom plugin development

---
+++ wp-content/plugins/clear-cut-vortex/admin/views/title.php
@@ -0,0 +1,4 @@
+<div class="centered vortex-box-header">
+	<h1><?php echo esc_html(get_admin_page_title()) ?></h1>
+	<h2><?php esc_html_e('Manage your your site and your content', 'cc-vortex'); ?></h2>
+</div>
---
+++ wp-content/plugins/clear-cut-vortex/src/blocks/search-form/index.js
@@ -0,0 +1,55 @@
+import { registerBlockType } from '@wordpress/blocks'
+import { useBlockProps, PanelColorSettings, InspectorControls } from '@wordpress/block-editor'
+import { __ } from '@wordpress/i18n'
+import block from './block.json'
+import icons from '../../icons'
+import './main.css'
+
+registerBlockType(block.name, {
+	icon: icons.primary,
+	edit({ attributes, setAttributes }) {
+		const { bgColor, textColor } = attributes
+		const blockProps = useBlockProps({
+			style: {
+				'background-color': bgColor,
+				color: textColor
+			}
+		})
+
+		return (
+			<>
+				<InspectorControls>
+					<PanelColorSettings
+						title={__('Colors', 'cc-vortex')}
+						colorSettings={[
+							{
+								label: __('Background Color', 'cc-vortex'),
+								value: bgColor,
+								onChange: newVal => setAttributes({bgColor: newVal})
+							},
+							{
+								label: __('Text Color', 'cc-vortex'),
+								value: textColor,
+								onChange: newVal => setAttributes({textColor: newVal})
+							}
+						]}
+
+					/>
+				</InspectorControls>
+				<div {...blockProps}>
+					<h1>Search: Your social search term here</h1>
+					<form>
+						<input type="text" placeholder="Search" />
+						<div className="btn-wrapper">
+							<button type="submit" style={{
+								'background-color': bgColor,
+								color: textColor
+							}}>Search</button>
+						</div>
+					</form>
+				</div>
+			</>
+		)
+	},
+
+})
