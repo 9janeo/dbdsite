d5573e0874df1af973ab461aee7fc1195d231d9e
Matt Adewole <matt.adewole@gmail.com>
2023-03-30 20:26:33 -0500

    add block and custom setting functionality to plugin

--- wp-content/plugins/clear-cut-vortex/build/blocks/search-form/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '34c05f6c6cf03b2e3259');
--- wp-content/plugins/clear-cut-vortex/includes/enqueue.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function ccv_enqueue()
-{
-	wp_register_style(
-		'ccv_admin_styles',
-		get_theme_file_uri('/assets/css/admin_styles.css'),
-	);
-}
