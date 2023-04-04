cda7c57db39b0ba17d71b55a3e799d2ae0e32a60
Matt Adewole <9janeo@gmail.com>
2023-04-04 04:58:40 -0500

    Generates a playlist url based on first video

--- wp-content/themes/dbdtheme/lib/youtube-templates/playlists.php
+++ wp-content/themes/dbdtheme/lib/youtube-templates/playlists.php
@@ -12,22 +12,23 @@ $description = $playlist->snippet->description;
 $thumbnails = $playlist->snippet->thumbnails;
 $itemCount =  $playlist->contentDetails->itemCount;
 $videos = $args['videos'];
+if (isset($videos[0])) {
+  $indexVid = $videos[0]->snippet->resourceId->videoId;
+  $playlistUrl = 'https://www.youtube.com/watch?v=' . $indexVid . '&list=' . $id;
+}
 // https://www.youtube.com/watch?v=DyOm5BX2GZk&list=PLgHIZMXekZto-3q1_Gb6HwE9fzfy3zF9g&ab_channel=DISBYDEM
-// Parameter for first video in playlist v=DyOm5BX2GZk
-// extract from thumbnail urls
-//  https://www.youtube.com/playlist?list=UUK8sQmJBp8GCxrOtXWBpyEA
-// print_r($playlist);
-// Embedd links
-// https://www.youtube.com/embed?list=PLMlNiWEoh5QrLU6C7_Ad71UtRpACh3FdQ
-// or
-// https://www.youtube.com/embed/?listType=playlist&list=PLMlNiWEoh5QrLU6C7_Ad71UtRpACh3FdQ
+
 ?>
 <div class="card">
   <div class="card-header">
     <h5 class="card-title"><?= $title ?></h5>
     <p class="card-text"><?php echo $itemCount; ?> Videos</p>
   </div>
-  <img class="card-img" src="<?php echo $thumbnails->medium->url ?>" />
+  <?php if (isset($playlistUrl)) : ?>
+    <a href="<?= $playlistUrl ?>" target="_blank"><img class="card-img" src="<?= $thumbnails->medium->url ?>" /></a>
+  <?php else : ?>
+    <img class="card-img" src="<?= $thumbnails->medium->url ?>" />
+  <?php endif; ?>
   <div class="card-footer">
     <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#list_<?= $id ?>" aria-expanded="false" aria-controls="multiCollapseVideoList">Toggle videos
     </button>
