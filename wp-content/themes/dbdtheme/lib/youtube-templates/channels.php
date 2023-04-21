23f3db9a2c2a2c00646ee299d17064f28b535d4a
Matt Adewole <matt.adewole@howarddc.com>
2023-04-21 16:46:03 -0500

    Separate new from edit channel settings

--- wp-content/themes/dbdtheme/lib/youtube-templates/channels.php
+++ wp-content/themes/dbdtheme/lib/youtube-templates/channels.php
@@ -1,31 +1,47 @@
 <div class="wrap">
   <h1>Channel Settings</h1>
   <div class="dbd-box">
+    <!-- <h3>Pull Channel Settings </h3> -->
+    <?php
+      $channel_settings = get_option('channel_settings');
+      // var_dump($channel_settings);
+    ?>
+    <?php // add new channel to channel_settings ?>
     <form role="presentation" action="options.php" method="post">
       <?php
       // output security fields
       settings_fields('channel_settings');
 
       // output setting sections
+      // print_r('Doing settings section new channel');
       do_settings_sections('dbd_channels');
+      // do_settings_sections('dbd_section_new_channel');
 
       // submit button
-      submit_button('Add Channel');
+      submit_button('Update Channels');
+      // submit_button('Add Channel');
 
       ?>
-
-    </form>
-    <form role="presentation" action="options.php" method="post">
+      <hr>
       <?php
-      // output security fields
-      settings_fields('dbd_options'); 
 
-      // output setting sections
-      do_settings_sections('dbd_admin_menu');
-
-      // submit button
-      submit_button();
-
+      // for each channel in settings => Iterate
+      // if(isset($channel_settings) && $channel_settings):?>
+      <!-- <div class="card"> -->
+        <?php
+          // foreach($channel_settings as $channel){
+          //   do_settings_sections('dbd_channels');
+          //   // submit button
+          //   submit_button('Update Channel');
+          // }
+        ?>
+      <!-- </div> -->
+      <?php
+      // else:
+      //   echo "<h3>cant find any channels</h3>";
+      //   do_settings_sections('dbd_channels');
+      //   submit_button('Add Channel');
+      // endif;
       ?>
 
     </form>
