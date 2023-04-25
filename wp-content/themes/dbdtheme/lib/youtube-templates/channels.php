6d418d875d7426a4251170df0d2d9e94237719a8
Matt Adewole <9janeo@gmail.com>
2023-04-25 05:39:23 -0500

    add tabs for single channel details

--- wp-content/themes/dbdtheme/lib/youtube-templates/channels.php
+++ wp-content/themes/dbdtheme/lib/youtube-templates/channels.php
@@ -3,10 +3,11 @@
   <div class="dbd-box">
     <!-- <h3>Pull Channel Settings </h3> -->
     <?php
-      $channel_settings = get_option('channel_settings');
-      // var_dump($channel_settings);
+
+    // var_dump($channel_settings);
     ?>
-    <?php // add new channel to channel_settings ?>
+    <?php // add new channel to channel_settings 
+    ?>
     <form role="presentation" action="options.php" method="post">
       <?php
       // output security fields
@@ -18,31 +19,61 @@
       // do_settings_sections('dbd_section_new_channel');
 
       // submit button
-      submit_button('Update Channels');
+      submit_button('Add Channel');
       // submit_button('Add Channel');
 
       ?>
       <hr>
-      <?php
-
-      // for each channel in settings => Iterate
-      // if(isset($channel_settings) && $channel_settings):?>
-      <!-- <div class="card"> -->
-        <?php
-          // foreach($channel_settings as $channel){
-          //   do_settings_sections('dbd_channels');
-          //   // submit button
-          //   submit_button('Update Channel');
-          // }
-        ?>
-      <!-- </div> -->
-      <?php
-      // else:
-      //   echo "<h3>cant find any channels</h3>";
-      //   do_settings_sections('dbd_channels');
-      //   submit_button('Add Channel');
-      // endif;
-      ?>
-
     </form>
+    <?php
+    if (DBD_Channels::get_channel_counts() > 0) :
+      $channel_settings = get_option('channel_settings');
+    ?>
+      <hr>
+      <!-- Create a header in the default WordPress 'wrap' container -->
+      <div class="wrap">
+
+        <div id="icon-themes" class="icon32"></div>
+        <h2>Channel Details</h2>
+        <?php
+        settings_errors();
+
+        var_dump($channel_settings);
+        ?>
+
+        <h2 class="nav-tab-wrapper">
+          <?php foreach ($channel_settings as $key => $channel) {
+            $name = $channel_settings[$key]['channel_name'] ? $channel_settings[$key]['channel_name'] : ($key . '_blank');
+          ?>
+            <a href="?page=dbd_channels&tab=<?= $name ?>" class="nav-tab"><?php echo 'Channel ' . ($key) . ' (' . $name . ')' ?></a>
+          <?php } ?>
+        </h2>
+
+        <!-- <form method="post" action=""> -->
+        <?php
+        /* foreach ($channel_settings as $key => $channel) {
+            add_settings_field(
+              'channel_name',
+              esc_html__('The channel Name', 'disbydem'),
+              array('DBD_Channels', 'dbd_channels_callback_field_text'),
+              'dbd_channels',
+              'dbd_section_channels',
+              ['channel' => $key, 'id' => 'channel_name', 'label' => esc_html__('Channel Name', 'disbydem')]
+            );
+
+            add_settings_field(
+              'channel_platform',
+              esc_html__('The channel platform', 'disbydem'),
+              array('DBD_Channels', 'dbd_callback_platform_select'),
+              'dbd_channels',
+              'dbd_section_channels',
+              ['channel' => $key, 'id' => 'channel_platform', 'label' => esc_html__('Channel platform', 'disbydem')]
+            );
+            submit_button('Save');
+            }  */
+        ?>
+        <!-- </form> -->
+
+      </div><!-- /.wrap -->
+    <?php endif; ?>
   </div>
