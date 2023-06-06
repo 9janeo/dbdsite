6d96e2c11a542db0197fedc008a6ce313e7d7e89
Matt Adewole <matt.adewole@howarddc.com>
2023-06-06 18:07:05 -0500

    clean up code in analytics file

--- wp-content/themes/dbdtheme/lib/youtube-templates/analytics.php
+++ wp-content/themes/dbdtheme/lib/youtube-templates/analytics.php
@@ -4,10 +4,8 @@ if (!current_user_can('manage_options')) {
 }
 
 if ($_GET["page"] === 'video-analytics' || is_page('videos')) {
-  // include_once('yt-service.php');
   wp_enqueue_script('custom-admin-script');
 }
-// $service = Dbd_Youtube::service();
 
 ?>
 <div class="wrap analytics">
@@ -30,30 +28,13 @@ if ($_GET["page"] === 'video-analytics' || is_page('videos')) {
         <?php
         if ($channel->platform == 'youtube') {
           $channel_id = $channel->channel_id;
-          // $channel_vids = Dbd_Youtube::get_channel_videos($channel);
-          // print_r($channel_vids);
         ?>
-        <!-- <h1>Youtube Analytics</h1> -->
-        <?php
-          // $videos = Dbd_Youtube::get_all_videos();
-          // var_dump($videos);
-        ?>
-        <h4>Playlists</h4>
+          <h4>Playlists</h4>
         <?php
           // Display the video analytics page
           $playlists = Dbd_Youtube::get_playlists_with_items($channel_id);
-
-          // foreach($playlists as $key => $playlist){
-          //   echo '<h3> Playlist ['. $key .'] '. $playlist->na
-          // }
-
-
-          // print_r($playlist);
-          // var_dump($playlists);
-          // if (isset($playlists->error)) :
           if (isset($playlists) && $playlists) :
             Dbd_Admin::display_playlists($playlists, true);
-            // print_r($playlist);
           endif;
         }
         ?>
@@ -61,27 +42,5 @@ if ($_GET["page"] === 'video-analytics' || is_page('videos')) {
     </div>
   <?php
   }
-
-  // xdebug_info();
-
-  // Dbd_Youtube::display_playlists($playlists);
-
-  /*
-  if (isset($playlists) && $playlists) {
-    if (!($playlists->error)) { ?>
-      <div class="yt playlists row row-cols-2">
-        <?php foreach ($playlists->items as $key => $playlist) {
-          $id = $playlist->id;
-          if (!($playlist->contentDetails->itemCount > 0)) {
-            // skip playlist if no items in it
-            continue;
-          }
-          $videos = DBD_Youtube::get_playlist_items($id);
-          // get the playlist items
-          get_template_part('lib/youtube-templates/playlists', 'playlists', array('playlist' => $playlist, 'videos' => $videos));
-        } ?>
-      </div>
-  <?php
-    }
-  } */ ?>
+?>
 </div>
