afac7f4ffecf2646ccfab04aaa0440cc102ae5e4
Matt Adewole <matt.adewole@howarddc.com>
2023-06-06 18:06:22 -0500

    add error handling to youtube classes

--- wp-content/themes/dbdtheme/lib/youtube-templates/analytics.php
+++ wp-content/themes/dbdtheme/lib/youtube-templates/analytics.php
@@ -10,49 +10,59 @@ if ($_GET["page"] === 'video-analytics' || is_page('videos')) {
 // $service = Dbd_Youtube::service();
 
 ?>
-<div class="wrap">
+<div class="wrap analytics">
   <h1>Channels</h1>
   <?php
   $channels = DBD_Channels::get_dbd_channels();
   foreach ($channels as $channel) { ?>
-    <div class="row">
-      <h2 class="header" data-bs-toggle="collapse" data-bs-target="#<?= $channel->channel_username ?>" aria-expanded="true" aria-controls="collapseTransients">
-        <?= $channel->channel_name ?>
-      </h2>
-      <span><?= $channel->platform ?> || <?= $channel->channel_url ?></span>
-      <hr>
-      <div id="<?= $channel->channel_username ?>" class="row collapse">
-        <p>Display the videos for this channel</p>
+    <div class="accordion-item">
+      <div class="accordion-header">
+        <div class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#<?= $channel->channel_username ?>" aria-expanded="false" aria-controls="<?= $channel->channel_username ?>">
+          <div class="col-6">
+            <h4><?= $channel->channel_name ?></h4>
+            <span class="small"><?= $channel->id . ' || ' . $channel->platform .' || '. $channel->channel_url ?></span>
+          </div>
+        </div>
+      </div>
+      
+      <div id="<?= $channel->channel_username ?>" class="p-3 accordion-collapse collapse">
+        <p class="mb-0">Display the videos for this channel</p>
         <?php
         if ($channel->platform == 'youtube') {
-          $channel_vids = Dbd_Youtube::get_channel_videos($channel);
-          print_r($channel_vids);
+          $channel_id = $channel->channel_id;
+          // $channel_vids = Dbd_Youtube::get_channel_videos($channel);
+          // print_r($channel_vids);
+        ?>
+        <!-- <h1>Youtube Analytics</h1> -->
+        <?php
+          // $videos = Dbd_Youtube::get_all_videos();
+          // var_dump($videos);
+        ?>
+        <h4>Playlists</h4>
+        <?php
+          // Display the video analytics page
+          $playlists = Dbd_Youtube::get_playlists_with_items($channel_id);
+
+          // foreach($playlists as $key => $playlist){
+          //   echo '<h3> Playlist ['. $key .'] '. $playlist->na
+          // }
+
+
+          // print_r($playlist);
+          // var_dump($playlists);
+          // if (isset($playlists->error)) :
+          if (isset($playlists) && $playlists) :
+            Dbd_Admin::display_playlists($playlists, true);
+            // print_r($playlist);
+          endif;
         }
         ?>
       </div>
     </div>
   <?php
   }
-  ?>
 
-  <h1>Youtube Analytics</h1>
-  <?php
-  $videos = Dbd_Youtube::get_all_videos();
-  // var_dump($videos);
-
-  ?>
-
-  <h2>Playlists</h2>
-  <?php
-  // Display the video analytics page
-  $playlists = Dbd_Youtube::get_playlists_with_items();
-
-  // var_dump($playlists);
-  if (isset($playlists) && $playlists) :
-    // $pl_vids = $playlists->
-    Dbd_Admin::display_playlists($playlists, false);
-  endif;
-
+  // xdebug_info();
 
   // Dbd_Youtube::display_playlists($playlists);
 
